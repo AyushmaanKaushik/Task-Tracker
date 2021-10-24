3ec334c01ed2516af1171910e42d354297da3bc9
AyushmaanKaushik <ayushmaan1362@gmail.com>
2021-10-24 12:45:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Do the thing",
+      day: "Feb 2nd at 2:30 PM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Do another thing",
+      day: "Feb 4th at 3:00 PM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Do one more thing",
+      day: "March 3rd at 11:00 AM",
+      reminder: false,
+    },
+  ]);
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No more tasks left!"
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.js
@@ -0,0 +1,18 @@
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      className="btn"
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+  text: "add",
+};
+
+export default Button;
